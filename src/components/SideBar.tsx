514f1ad24fbc0f1e800bdeec860fda1cb1776358
reversonthaiam <reversonthayan@gmail.com>
2021-05-15 10:13:58 -0300

    separação de componentes pronto

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,47 @@
-export function SideBar() {
-  // Complete aqui
+import {useState, useEffect} from 'react'
+import {Button} from './Button'
+
+import { api } from '../services/api';
+import '../styles/sidebar.scss';
+
+interface SideBarIndicador{
+  onClick(id: number):void;
+  selected: number;
+}
+
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+export function SideBar({onClick, selected} : SideBarIndicador) {
+  
+  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
+
+  useEffect(() => {
+    api.get<GenreResponseProps[]>('genres').then(response => {
+      setGenres(response.data);
+    });
+  }, []);
+
+  return(
+    <nav className="sidebar">
+        <span>Watch<p>Me</p></span>
+
+        <div className="buttons-container">
+          {genres.map(genre => (
+            <Button
+              key={String(genre.id)}
+              title={genre.title}
+              iconName={genre.name}
+              onClick={() => onClick(genre.id)}
+              selected={selected === genre.id}
+            />
+          ))}
+        </div>
+
+      </nav>
+  )
 }
